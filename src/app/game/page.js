fe8e349fd2f96d39947b1f97281d1c83c69d914a
Anointed Osara <anointedosara@gmail.com>
2025-12-18 10:00:25 +0100

    first commit

--- src/app/game/page.js
+++ src/app/game/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 function page() {
   return (
-    <div className='bg-[#FCFCFC] py-10 p-5 md:py-5 flex items-start md:items-center justify-center'>
+    <div className='bg-[#FCFCFC] py-10 p-5 md:py-5 flex items-start md:items-center w-screen h-screen justify-center'>
         <Game />
     </div>
   )
